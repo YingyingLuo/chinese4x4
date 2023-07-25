862f957ad5fc475334a22fcad6d9f6099e794a50
Yingying Luo <yingyingbluo@gmail.com>
2023-07-25 16:37:08 -0700

    Made Card id an optional parameter

--- website/dragdrop.py
+++ website/dragdrop.py
@@ -6,8 +6,12 @@ def mouseover(event):
 def dragstart(event):
     event.dataTransfer.setData("dragged", event.target.id)  # data has to be a string
 
+builtin_id = id
+
 class Card(html.SPAN):
-    def __init__(self, text, Class=None):
-        super(self.__class__, self).__init__(text, id=f"id_{id(self)}", Class=Class, draggable=True)
+    def __init__(self, text, id=None, Class=None):
+        if not id:
+            id = f"id_{builtin_id(self)}"
+        super(self.__class__, self).__init__(text, id=id, Class=Class, draggable=True)
         self.bind("mouseover", mouseover)
         self.bind("dragstart", dragstart)
